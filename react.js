2eceb05f1f6e403bd65c8c6701ddbd82bb44231d
Kevin Winkelman <kevwinkelman@gmail.com>
2018-01-18 12:42:36 -0800

    Initial
    Designed the CSS and where the quotes may be displayed

---
+++ react.js
@@ -0,0 +1,16 @@
+var movieQuotes = [{
+        quote: "Frankly, my dear, I don't give a damn.",
+        name: "RHETT BUTLER",
+    },
+
+    {
+        quote: "I'm going to make him an offer he can't refuse.",
+        name: "VITO CORLEONE",
+    },
+
+    {
+        quote: "You don't understand! I could've had class. I could've been a contender. I could've been somebody, instead of a bum, which is what I am." ,
+        name: "TERRY MALLOY",
+    }
+
+]
